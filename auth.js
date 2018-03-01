33f3c37683c64428e808ed8f56bbbdd8fe6b11cf
Alvin <tobteks@gmail.com>
2018-03-01 10:08:03 +0000

    added comment

--- auth.js
+++ auth.js
@@ -3,6 +3,6 @@ module.exports={
     'googleAuth':{
         'clientID':'296627793404-eidvrntjp0jcphekmd07goq29kg5a633.apps.googleusercontent.com',
         'clientSecret':'rR8DIC5BotuqgKe2D6Se0q7D',
-        'callbackURL':'https://traffichit.herokuapp.com/auth/google/callback'
+        'callbackURL':'http://localhost:8080/auth/google/callback'//https://traffichit.herokuapp.com/auth/google/callback
     }
 }
